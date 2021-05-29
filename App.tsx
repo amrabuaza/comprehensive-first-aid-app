ad9e7a4505665d8a3eb5c206bb506f7047492f67
hasan_1995 <hasan.khaled.alawneh@gmail.com>
2021-05-29 13:24:19 +0300

    last chages

--- App.tsx
+++ App.tsx
@@ -1,22 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MainNavigation from './src/navigation/main-navigation';
 import User from "./src/context/user";
 import * as Location from 'expo-location';
+import * as Application from 'expo-application';
+import * as Font from 'expo-font';
+import { Alert } from 'react-native';
 
 export default function App() {
+
+  // state.
+  const [isSetupCompleted, setIsSetupCompleted] = useState(false);
 
   /**
    * On load app check location permissions if not granted
    * need to ask user to geant permissions
    */
   useEffect(() => {
-    Location.getForegroundPermissionsAsync().then((premissions) => {
-      if (!premissions.granted) {
-        Location.requestForegroundPermissionsAsync();
+    (async function () {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('You should grant a location permission, plesae enabe it from settings.')
+        return;
       }
-    })
+
+      setIsSetupCompleted(true);
+    })();
   }, [])
 
+  if (!isSetupCompleted) return null;
   return (
     <User.Provider>
       <MainNavigation />
